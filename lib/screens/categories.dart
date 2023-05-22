6ed302ef58758f70b81180b1bfebd884c9eacf17
abhishekdwibedy-2002 <dwibedyabhishek1@gmail.com>
2023-05-22 17:35:08 +0530

    Final UI Updated With Animations...

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -26,7 +26,17 @@ class _CategoriesScreenState extends State<CategoriesScreen>
     super.initState();
     animationController = AnimationController(
       vsync: this,
+      duration: const Duration(milliseconds: 240),
+      lowerBound: 0,
+      upperBound: 1,
     );
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 
   void _selectCategory(BuildContext context, Category category) {
@@ -46,24 +56,44 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
       ),
-      children: [
-        // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
-          )
-      ],
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: animationController,
+            curve: Curves.easeIn,
+          ),
+        ),
+        child: child,
+      ),
+
+      //Padding(
+      //   padding: EdgeInsets.only(top: 100 - animationController.value * 100),
+      //   child: child,
+      // ),
     );
   }
 }
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,6 +18,7 @@ class MealDetailsScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final a = ref.watch(favoriteMealsProvider);
     final isFav = a.contains(meal);
+
     return Scaffold(
         appBar: AppBar(title: Text(meal.title), actions: [
           IconButton(
@@ -27,72 +30,101 @@ class MealDetailsScreen extends ConsumerWidget {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
-                      wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
+                      wasAdded ? 'Meal Added As Favorite.!!' : 'Meal Removed'),
                 ),
               );
             },
-            icon: Icon(isFav ? Icons.star : Icons.star_border_outlined),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.9, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFav ? Icons.star : Icons.star_border_outlined,
+                key: ValueKey(isFav),
+              ),
+            ),
           )
         ]),
         body: Container(
           constraints: const BoxConstraints.expand(),
           decoration: BoxDecoration(
             image: DecorationImage(
-                image: NetworkImage(meal.imageUrl),
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(0.7), BlendMode.dstATop)),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                // Image.network(
-                //   meal.imageUrl,
-                //   height: 300,
-                //   width: double.infinity,
-                //   fit: BoxFit.cover,
-                // ),
-                const SizedBox(height: 14),
-                Text(
-                  'Ingredients',
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const SizedBox(height: 14),
-                for (final ingredient in meal.ingredients)
-                  Text(
-                    ingredient,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                  ),
-                const SizedBox(height: 24),
-                Text(
-                  'Steps',
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const SizedBox(height: 14),
-                for (final step in meal.steps)
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 12,
-                      vertical: 8,
-                    ),
-                    child: Text(
-                      step,
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            color: Theme.of(context).colorScheme.onBackground,
-                          ),
-                    ),
-                  ),
-              ],
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
             ),
+          ),
+          child: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.black45,
+                  child: Column(
+                    children: [
+                      // Image.network(
+                      //   meal.imageUrl,
+                      //   height: 300,
+                      //   width: double.infinity,
+                      //   fit: BoxFit.cover,
+                      // ),
+                      //const SizedBox(height: 14),
+                      Text(
+                        'Ingredients',
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      const SizedBox(height: 14),
+                      for (final ingredient in meal.ingredients)
+                        Text(
+                          ingredient,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                              ),
+                        ),
+                      const SizedBox(height: 24),
+                      Text(
+                        'Steps',
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      const SizedBox(height: 14),
+                      for (final step in meal.steps)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 8,
+                          ),
+                          child: Text(
+                            step,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onBackground,
+                                ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ));
   }
